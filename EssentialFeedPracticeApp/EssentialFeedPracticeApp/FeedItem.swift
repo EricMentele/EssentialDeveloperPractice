0777fa472d06d003c5fae088ee579771b6d7f947
Eric Mentele <ericmentele@gmail.com>
2023-12-18 14:47:06 -0600

    Create Feed Item model
    FeedItem

---
+++ EssentialFeedPracticeApp/EssentialFeedPracticeApp/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeedPracticeApp
+//
+//  Created by Eric Mentele on 12/18/23.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
