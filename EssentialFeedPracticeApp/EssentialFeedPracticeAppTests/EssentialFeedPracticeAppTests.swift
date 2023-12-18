70d19c776be01e0ccfd882517bd77912e9320ccd
Eric Mentele <ericmentele@gmail.com>
2023-12-18 14:33:32 -0600

    Create essential developer practice framework

---
+++ EssentialFeedPracticeApp/EssentialFeedPracticeAppTests/EssentialFeedPracticeAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  EssentialFeedPracticeAppTests.swift
+//  EssentialFeedPracticeAppTests
+//
+//  Created by Eric Mentele on 12/18/23.
+//
+
+import XCTest
+@testable import EssentialFeedPracticeApp
+
+final class EssentialFeedPracticeAppTests: XCTestCase {
+    func testExample() throws {
+        let sut = "Hello World"
+        XCTAssertEqual(sut, "Hello World")
+        XCTAssertNotEqual(sut, "Foobar")
+    }
+}
