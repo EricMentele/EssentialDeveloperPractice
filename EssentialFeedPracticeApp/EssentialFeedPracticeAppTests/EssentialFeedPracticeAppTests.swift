0c89d42482a98f1c6cb83e95d474b863f9197f24
Eric Mentele <ericmentele@gmail.com>
2023-12-18 18:20:12 -0600

    Inject url into RemoteFeedLoader instead of hard coding and test that the HTTPClientSpy has the same url

--- EssentialFeedPracticeApp/EssentialFeedPracticeAppTests/EssentialFeedPracticeAppTests.swift
+++ EssentialFeedPracticeApp/EssentialFeedPracticeAppTests/EssentialFeedPracticeAppTests.swift
@@ -10,13 +10,15 @@ import XCTest
 
 class RemoteFeedLoader {
     let client: HTTPClient
+    let url: URL
     
-    init(client: HTTPClient) {
+    init(client: HTTPClient, url: URL) {
         self.client = client
+        self.url = url
     }
     
     func load() {
-        client.get(from: URL(string: "https://a-given-url.com")!)
+        client.get(from: url)
     }
 }
 
@@ -35,17 +37,19 @@ class HTTPClientSpy: HTTPClient {
 final class EssentialFeedPracticeAppTests: XCTestCase {
     func test_init_doesNotRequestDataFromURL() {
         let client = HTTPClientSpy()
-        _ = RemoteFeedLoader(client: client)
+        let url = URL(string: "https://a-given-url.com")!
+        _ = RemoteFeedLoader(client: client, url: url)
         
         XCTAssertNil(client.requestedURL)
     }
     
     func test_load_requestDataFromURL() {
         let client = HTTPClientSpy()
-        let sut = RemoteFeedLoader(client: client)
+        let url = URL(string: "https://a-given-urlaaaaaa.com")!
+        let sut = RemoteFeedLoader(client: client, url: url)
         
         sut.load()
         
-        XCTAssertNotNil(client.requestedURL)
+        XCTAssertEqual(client.requestedURL, url)
     }
 }
