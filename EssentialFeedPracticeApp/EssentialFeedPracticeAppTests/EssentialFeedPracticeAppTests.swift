c76653abd007ddb9001520d771cf02a2d317f85d
Eric Mentele <ericmentele@gmail.com>
2023-12-22 20:26:14 -0600

    Refactor helper to receive Result instead of Error types to enable reusability for different potential results

--- EssentialFeedPracticeApp/EssentialFeedPracticeAppTests/EssentialFeedPracticeAppTests.swift
+++ EssentialFeedPracticeApp/EssentialFeedPracticeAppTests/EssentialFeedPracticeAppTests.swift
@@ -37,14 +37,11 @@ final class EssentialFeedPracticeAppTests: XCTestCase {
     
     func test_load_deliversNoItemsOn200HTTPResponseWithEmptyJSONList() {
         let (sut, client) = makeSUT()
-        
-        var capturedResults = [RemoteFeedLoader.Result]()
-        sut.load { capturedResults.append($0) }
-        
-        let emptyListJSON = Data("{\"items\": []}".utf8)
-        client.complete(withStatusCode: 200, data: emptyListJSON)
-        
-        XCTAssertEqual(capturedResults, [.success([])])
+
+        expect(sut: sut, toCompleteWith: .success([]), when: {
+            let emptyListJSON = Data("{\"items\": []}".utf8)
+            client.complete(withStatusCode: 200, data: emptyListJSON)
+        })
     }
     
     // MARK: Not Happy Path
@@ -53,7 +50,7 @@ final class EssentialFeedPracticeAppTests: XCTestCase {
         // Arrange
         let (sut, client) = makeSUT()
         // Act & Assert
-        expect(sut: sut, toCompleteWith: .connectivity, when: {
+        expect(sut: sut, toCompleteWith: .failure(.connectivity), when: {
             let clientError = NSError(domain: "a client error", code: 0)
             client.complete(with: clientError)
         })
@@ -65,7 +62,7 @@ final class EssentialFeedPracticeAppTests: XCTestCase {
         let samples = [199, 201, 300, 400, 500]
         
         samples.enumerated().forEach { index, code in
-            expect(sut: sut, toCompleteWith: .invalidData, when: {
+            expect(sut: sut, toCompleteWith: .failure(.invalidData), when: {
                 client.complete(withStatusCode: code, at: index)
             })
         }
@@ -74,7 +71,7 @@ final class EssentialFeedPracticeAppTests: XCTestCase {
     func test_load_deliversErrorOn200HTTPResponseWithInvalidJSON() {
         let (sut, client) = makeSUT()
         
-        expect(sut: sut, toCompleteWith: .invalidData, when: {
+        expect(sut: sut, toCompleteWith: .failure(.invalidData), when: {
             let invalidJSON = Data("invalid json".utf8)
             client.complete(withStatusCode: 200, data: invalidJSON)
         })
@@ -83,15 +80,17 @@ final class EssentialFeedPracticeAppTests: XCTestCase {
 
 private func expect(
     sut: RemoteFeedLoader,
-    toCompleteWith error: RemoteFeedLoader.Error,
+    toCompleteWith result: RemoteFeedLoader.Result,
     when action: () -> Void,
     file: StaticString = #filePath,
     line: UInt = #line
 ) {
     var capturedResults = [RemoteFeedLoader.Result]()
     sut.load { capturedResults.append($0) }
+    
     action()
-    XCTAssertEqual(capturedResults, [.failure(error)], file: file, line: line)
+    
+    XCTAssertEqual(capturedResults, [result], file: file, line: line)
 }
 
 private func makeSUT(url: URL = URL(string: "https://a-given-url.com")!) -> (sut: RemoteFeedLoader, client: HTTPClientSpy) {
