768184fe85f5d3f265186594611d685c171248f8
Eric Mentele <ericmentele@gmail.com>
2024-02-22 10:18:00 -0600

    Save command fails on cache insertion error

--- EssentialFeedPracticeApp/EssentialFeedPracticeAppTests/Feed Cache/CacheFeedUseCaseTests.swift
+++ EssentialFeedPracticeApp/EssentialFeedPracticeAppTests/Feed Cache/CacheFeedUseCaseTests.swift
@@ -19,9 +19,10 @@ class LocalFeedLoader {
     
     func save(_ items: [FeedItem], completion: @escaping (Error?) -> Void) {
         store.deleteCachedFeed { [unowned self] error in
-            completion(error)
             if error == nil {
-                self.store.insert(items, timestamp: currentDate())
+                self.store.insert(items, timestamp: currentDate(), completion: completion)
+            } else {
+                completion(error)
             }
         }
     }
@@ -29,6 +30,7 @@ class LocalFeedLoader {
 
 class FeedStore {
     typealias DeletionCompletion = (Error?) -> Void
+    typealias InsertionCompletion = (Error?) -> Void
     
     enum RecievedMessage: Equatable {
         case deleteCachedFeed
@@ -38,6 +40,7 @@ class FeedStore {
     var recievedMessages = [RecievedMessage]()
     
     private var deletionCompletions = [DeletionCompletion]()
+    private var insertionCompletions = [InsertionCompletion]()
     
     func deleteCachedFeed(completion: @escaping (Error?) -> Void) {
         deletionCompletions.append(completion)
@@ -52,8 +55,13 @@ class FeedStore {
         deletionCompletions[index](nil)
     }
     
-    func insert(_ items: [FeedItem], timestamp: Date) {
+    func insert(_ items: [FeedItem], timestamp: Date, completion: @escaping InsertionCompletion) {
+        insertionCompletions.append(completion)
         recievedMessages.append(.insert(items, timestamp))
+    }
+    
+    func completeInsertion(with error: Error, at index: Int = 0) {
+        insertionCompletions[index](error)
     }
 }
 
