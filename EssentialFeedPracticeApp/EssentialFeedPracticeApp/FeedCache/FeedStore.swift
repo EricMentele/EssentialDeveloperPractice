e0d157d455327fea1b5b6f25c6b32d1870b4a84e
Eric Mentele <ericmentele@gmail.com>
2024-02-27 22:42:21 -0600

    Move `LocalFeedItem` to it's own file

--- EssentialFeedPracticeApp/EssentialFeedPracticeApp/FeedCache/FeedStore.swift
+++ EssentialFeedPracticeApp/EssentialFeedPracticeApp/FeedCache/FeedStore.swift
@@ -14,22 +14,3 @@ public protocol FeedStore {
     func deleteCachedFeed(completion: @escaping (Error?) -> Void)
     func insert(_ items: [LocalFeedItemDTO], timestamp: Date, completion: @escaping InsertionCompletion)
 }
-
-public struct LocalFeedItemDTO: Equatable {
-    public let id: UUID
-    public let description: String?
-    public let location: String?
-    public let imageURL: URL
-    
-    public init(
-        id: UUID,
-        description: String?,
-        location: String?,
-        imageURL: URL
-    ) {
-        self.id = id
-        self.description = description
-        self.location = location
-        self.imageURL = imageURL
-    }
-}
