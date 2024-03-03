388f048256496d54982e77de82a37ff56f4e6f17
Eric Mentele <ericmentele@gmail.com>
2024-03-03 16:32:27 -0600

    Deleting non-empty cache empties cache

--- EssentialFeedPracticeApp/EssentialFeedPracticeAppTests/Feed Cache/CodableFeedStoreTests.swift
+++ EssentialFeedPracticeApp/EssentialFeedPracticeAppTests/Feed Cache/CodableFeedStoreTests.swift
@@ -71,10 +71,14 @@ class CodableFeedStore {
         } catch {
             completion(error)
         }
-        
     }
     
     func deleteCacheFeed(completion: @escaping FeedStore.DeletionCompletion) {
+        guard FileManager.default.fileExists(atPath: storeURL.path) else {
+            return completion(nil)
+        }
+        
+        try! FileManager.default.removeItem(at: storeURL)
         completion(nil)
     }
 }
@@ -184,6 +188,28 @@ final class CodableFeedStoreTests: XCTestCase {
         wait(for: [exp], timeout: 1.0)
         
         expect(sut, toRetrieve: .empty)
+    }
+    
+    func test_delete_emptiesPreviouslyInsertedCache() {
+        let sut = makeSUT()
+        let feed = uniqueImageFeed().local
+        let timestamp = Date()
+        
+        insert((feed, timestamp), to: sut)
+        
+        let exp = expectation(description: "Wait for cache deletion.")
+        sut.deleteCacheFeed { deletionError in
+            XCTAssertNil(deletionError, "Expected empty cache deletion to succeed")
+            exp.fulfill()
+        }
+        
+        wait(for: [exp], timeout: 1.0)
+        
+        expect(sut, toRetrieve: .empty)
+    }
+    
+    func test_delete_deliversErrorOnDeletionError() {
+        
     }
 }
 
