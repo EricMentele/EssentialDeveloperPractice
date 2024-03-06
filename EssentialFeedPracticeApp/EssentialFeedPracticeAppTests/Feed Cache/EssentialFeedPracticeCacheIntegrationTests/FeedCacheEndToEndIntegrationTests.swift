bc4d608f1c29eb601ad0385733482af58aeabb60
Eric Mentele <ericmentele@gmail.com>
2024-03-06 11:46:24 -0600

    Add EssentialFeedCacheIntegrationTests target to separate the potentially-slower cache integration tests from the fast unit/isolated tests

---
+++ EssentialFeedPracticeApp/EssentialFeedPracticeAppTests/Feed Cache/EssentialFeedPracticeCacheIntegrationTests/FeedCacheEndToEndIntegrationTests.swift
@@ -0,0 +1,13 @@
+//
+//  FeedCacheEndToEndIntegrationTests.swift
+//  EssentialFeedPracticeCacheIntegrationTests
+//
+//  Created by Eric Mentele on 3/6/24.
+//
+
+import XCTest
+
+final class FeedCacheEndToEndIntegrationTests: XCTestCase {
+    
+
+}
