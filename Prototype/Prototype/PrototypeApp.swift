f873dd176ceaf02c97a43b20c3a87ae632908439
Eric Mentele <ericmentele@gmail.com>
2024-03-11 13:05:42 -0500

    Create prototype project

---
+++ Prototype/Prototype/PrototypeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PrototypeApp.swift
+//  Prototype
+//
+//  Created by Eric Mentele on 3/11/24.
+//
+
+import SwiftUI
+
+@main
+struct PrototypeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
