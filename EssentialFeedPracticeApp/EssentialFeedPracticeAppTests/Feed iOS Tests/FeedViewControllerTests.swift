7059829a40e006245b9e9717016f91a362d6b764
Eric Mentele <ericmentele@gmail.com>
2024-03-13 19:17:10 -0500

    Hide loading indicator on loader completion

--- EssentialFeedPracticeApp/EssentialFeedPracticeAppTests/Feed iOS Tests/FeedViewControllerTests.swift
+++ EssentialFeedPracticeApp/EssentialFeedPracticeAppTests/Feed iOS Tests/FeedViewControllerTests.swift
@@ -31,7 +31,9 @@ final class FeedViewController: UITableViewController {
     }
     
     @objc private func load() {
-        loader?.load { _ in }
+        loader?.load { [weak self] _ in
+            self?.refreshControl?.endRefreshing()
+        }
     }
     
     @objc func startRefreshIfViewHasNotAppearedBefore() {
@@ -81,6 +83,15 @@ final class FeedViewControllerTests: XCTestCase {
         
         XCTAssertEqual(sut.refreshControl?.isRefreshing, true)
     }
+    
+    func test_viewDidLoad_hidesLoadingIndicatorOnLoaderCompletion() {
+        let (sut, loader) = makeSUT()
+        
+        sut.loadViewIfNeeded()
+        loader.completeFeedLoading()
+        
+        XCTAssertEqual(sut.refreshControl?.isRefreshing, false)
+    }
 }
 
 // MARK: - Helpers
@@ -95,10 +106,18 @@ private extension FeedViewControllerTests {
     }
     
     class LoaderSpy: FeedLoader {
-        private(set) var loadCallCount: Int = 0
+        private var completions = [(FeedLoader.Result) -> Void]()
+        
+        var loadCallCount: Int {
+            return completions.count
+        }
         
         func load(completion: @escaping (FeedLoader.Result) -> Void) {
-            loadCallCount += 1
+            completions.append(completion)
+        }
+        
+        func completeFeedLoading() {
+            completions[0](.success([]))
         }
     }
     
