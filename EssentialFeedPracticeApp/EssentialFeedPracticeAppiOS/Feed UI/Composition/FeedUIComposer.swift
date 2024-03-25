7b97cd545dc2876897193333b66aabc92f4b2285
Eric Mentele <ericmentele@gmail.com>
2024-03-25 17:25:06 -0500

    Move `WeakRefVirtualProxy` into it's own file

--- EssentialFeedPracticeApp/EssentialFeedPracticeAppiOS/Feed UI/Composition/FeedUIComposer.swift
+++ EssentialFeedPracticeApp/EssentialFeedPracticeAppiOS/Feed UI/Composition/FeedUIComposer.swift
@@ -116,27 +116,3 @@ private extension FeedViewController {
         return feedController
     }
 }
-
-// MARK: Helpers
-
-private final class WeakRefVirtualProxy<T: AnyObject> {
-    private weak var object: T?
-    
-    init(_ object: T) {
-        self.object = object
-    }
-}
-
-extension WeakRefVirtualProxy: FeedLoadingView where T: FeedLoadingView {
-    func display(_ viewModel: FeedLoadingViewModel) {
-        object?.display(viewModel)
-    }
-}
-
-extension WeakRefVirtualProxy: FeedImageView where T: FeedImageView, T.Image == UIImage {
-    func display(_ model: FeedImageViewModel<UIImage>) {
-        object?.display(model)
-    }
-}
-
-
